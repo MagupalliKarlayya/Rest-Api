0d8a2ed1bd737f4701a9bdc4ebced73d1a1413e3
MagupalliKarlayya <mkarlayya@gmail.com>
2024-07-28 10:13:23 +0530

    add a new url

--- keep-alive.cjs
+++ keep-alive.cjs
@@ -3,7 +3,7 @@ const cron = require("node-cron");
 const axios = require("axios");
 
 // Replace with your URL
-const keepAliveUrl = "https://foodfantacy-app.netlify.app/";
+const keepAliveUrl = "https://foodfantacy.onrender.com/api/swiggy";
 
 // Schedule the script to run every 5 minutes
 cron.schedule("*/5 * * * *", async () => {
